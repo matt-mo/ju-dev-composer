6a663f76f0e5a675e4fa332d03c2a1a24f7bb213
matt-mo <matt.m@justuno.com>
2018-12-13 13:28:30 -0600

    Added files pushed up

---
+++ Controller/Index/Index.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Justuno offers optimized marketing campaigns with customizable design, powerful targeting & segmentation, and A/B testing.
+ * Copyright (C) 2018 
+ * 
+ * This file is part of Justuno/Integration.
+ * 
+ * Justuno/Integration is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Justuno\Integration\Controller\Index;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+
+    protected $resultPageFactory;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Framework\App\Action\Context  $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Execute view action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
+}
---
+++ Setup/UpgradeSchema.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Justuno offers optimized marketing campaigns with customizable design, powerful targeting & segmentation, and A/B testing.
+ * Copyright (C) 2018 
+ * 
+ * This file is part of Justuno/Integration.
+ * 
+ * Justuno/Integration is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Justuno\Integration\Setup;
+
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\UpgradeSchemaInterface;
+
+class UpgradeSchema implements UpgradeSchemaInterface
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function upgrade(
+        SchemaSetupInterface $setup,
+        ModuleContextInterface $context
+    ) {
+        if (version_compare($context->getVersion(), "1.0.0", "<")) {
+            //Your upgrade script
+        }
+
+        if (version_compare($context->getVersion(), "1.0.1", "<")) {
+            //Your upgrade script
+        }
+
+        if (version_compare($context->getVersion(), "1.0.2", "<")) {
+            //Your upgrade script
+        }
+    }
+}
